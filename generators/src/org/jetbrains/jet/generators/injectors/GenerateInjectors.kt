b95be1369d8e7d813d59bb2d40e452be83b5ceaa
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2014-12-30 22:02:41 +0300

    Frontend: AdditionalCheckerProvider.Empty->AdditionalCheckerProvider.DefaultProvider

--- generators/src/org/jetbrains/jet/generators/injectors/GenerateInjectors.kt
+++ generators/src/org/jetbrains/jet/generators/injectors/GenerateInjectors.kt
@@ -103,7 +103,7 @@ private fun generatorForLazyTopDownAnalyzerBasic() =
 
             publicField<LazyTopDownAnalyzer>()
 
-            field<AdditionalCheckerProvider.Empty>()
+            field<AdditionalCheckerProvider.DefaultProvider>()
         }
 
 private fun generatorForLazyBodyResolve() =
@@ -215,7 +215,7 @@ private fun generatorForMacro() =
             field<GlobalContext>(useAsContext = true,
                   init = GivenExpression("org.jetbrains.jet.context.ContextPackage.GlobalContext()"))
 
-            field<AdditionalCheckerProvider.Empty>()
+            field<AdditionalCheckerProvider.DefaultProvider>()
         }
 
 private fun generatorForTests() =
